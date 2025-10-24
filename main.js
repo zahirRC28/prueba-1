86fe82cc95eb2fa28da08f264b5791dad11c0274
zahirRC28 <zahirvalentino2812@gmail.com>
2025-10-24 09:50:02 +0200

    correcciones afinando detalles de nombres y interaccion

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 //Ejercicio 1
 /* Declara una función que acepte una palabra como parámetro y la devuelva con todos sus caracteres en mayúsculas 
 y separados por espacios. Ejemplo: Para "Ana" devolverá "A N A".*/
-//let palabra = prompt();
-let palabra = "Peru";
+let palabra = prompt();
+//let palabra = "Peru";
 
 
 function dividirPa(palabra){
@@ -19,8 +19,8 @@ console.log(dividirPa(palabra));
 //Ejercicio 2
 /*Declara una función que acepte un nombre como parámetro y 
 devuelva el número de palabras que tiene el nombre. Ejemplo: Para "Antonio Alberto Jesús" devolverá 3.*/
-//let frase = prompt();
-let frase = "Hola que tal"
+let frase = prompt();
+//let frase = "Hola que tal"
 
 function contarFra(frase){
     let guardar = frase.toString().split(" ");
@@ -31,8 +31,10 @@ console.log(contarFra(frase));
 
 //Ejercicio 3
 /*Diseña un algoritmo que cuente las veces que aparece una determinada letra en una frase.*/
-let frase2 = "prueba prueba aguacate"
-let letraFra = "a"
+let frase2 = prompt();
+let letraFra = prompt();
+//let frase2 = "prueba prueba aguacate"
+//let letraFra = "a"
 function queLetra(frase2, letraFra){
     let almacen = [];
     let guardar = frase2.toString().split('');
@@ -50,14 +52,16 @@ console.log(queLetra(frase2,letraFra));
         leer frase2
         Leer letraFra
         Ejecuta queLetra
+        pilla variable frase2
+        pilla variable letraFra
         crea almacen
         separa frase en array
         lee guardar
         mientras i sea menor que la cantidad de datos de guardar
             si guardar posicion i es igual a letraFra
-                alamcena guardar posicion i en alamacen
+                almacena guardar posicion i en almacen
         lee almacen
-        cuanta los datos dentro de alamacen
-        envia los datos de alamcen
+        cuanta los datos dentro de almacen
+        envia los datos de almacen
     Final
 */
